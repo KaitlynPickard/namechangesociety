91802cf82133ad7986ec9983b2d6a4bd49e08a34
Kaitlyn Pickard <kaitlynpickard@gmail.com>
2018-01-22 18:10:21 +1000

    Fixing optional params paths

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 })
 
-app.get('/getUserName/:userID/:date?*', users.getUserName);
+app.get('/getUserName/:userID/:date?', users.getUserName);
 app.get('/getUserNameHistory/:userID', users.getUserNameHistory);
 app.get('/getUserQueuedName/:userID', users.getUserQueuedName);
 app.get('/checkIfCurrentNameExpired/:userID', users.checkIfCurrentNameExpired);
 app.get('/getExpiringNames/:userID', users.getExpiringNames);
-app.get('/setUserName/:userID/:startDate?*', users.setUserName);
+app.get('/setUserName/:userID/:startDate?', users.setUserName);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
