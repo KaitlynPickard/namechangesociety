cbc4294a0750527e5ba8b4a3c581f6731189621a
Kaitlyn Pickard <kaitlynpickard@gmail.com>
2018-01-23 20:53:42 +1000

    Added functionality to create a salt and hashed password for a user

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/getAllUsersNames', admin.getAllUsersNames);
 app.get('/getAllQueuedNames', admin.getAllQueuedNames);
 app.get('/getExpiredNames', admin.getExpiredNames);
 
-app.get('/createUser', auth.createUser);
+app.get('/createUser/:password', auth.createUser);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
--- server/auth.js
+++ server/auth.js
@@ -1,21 +1,30 @@
 let db = require('./db');
+let crypto = require('crypto');
 let shortid = require('shortid');
-let todaysDate = new Date();
 
 // let escape = s => s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 
 let createUser = (req, res) => {
-	// let plainTextPassword = req.params.password;
-	let userID = createUserID()
-		.then(result => {
-			return res.send(result.toString())
+	let plainTextPassword = req.params.password;
+	let newUser = createUserID()
+		.then(userID => {
+			let newSalt = createSalt()
+			.then(salt => {
+				let hashedPassword = createHashedPassword(plainTextPassword, salt);
+				return res.send(userID.toString() + " : " + salt.toString() + " : " + hashedPassword);
+			})
+			.catch(function () {
+				console.log("Promise Rejected");
+			});
+			// return res.send(result.toString())
 		})
 		.catch(function () {
 			console.log("Promise Rejected");
 		});
 }
 
-function createUserID() {	
+function createUserID() {
+	// TODO make sure this doesn't include special characters
 	let userID = shortid.generate(); // generates random id that is 9 characters long and is a mix of number and letters - eg. Sk7l1q4Bf
 
 	return new Promise((resolve, reject) => {
@@ -34,11 +43,31 @@ function createUserID() {
 }
 
 function createSalt() {
+	let saltLength = 32;
+	let salt = crypto.randomBytes(Math.ceil(saltLength/2))
+        .toString('hex')
+        .slice(0,saltLength);
 
+	return new Promise((resolve, reject) => {
+		let result = db.query('SELECT COUNT("SALT") as count FROM public."USERS" WHERE "SALT" = $1', [salt])
+			.then(result => {
+				if (result[0].count == 0) {
+					resolve(salt);
+				} else {
+					return createSalt();
+				}
+			})
+			.catch(function () {
+ 				console.log("Promise Rejected");
+			});
+	});
 }
 
-function createHashedPassword(password) {
-
+function createHashedPassword(password, salt) {
+	let hashedPassword = crypto.createHmac('sha256', salt)
+       .update(password)
+       .digest('hex');
+	return hashedPassword;
 }
 
 let authenticateUser = (req, res) => {
