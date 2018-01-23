228443483f1f73b1fca10b7a586a72c132fce1cd
Kaitlyn Pickard <kaitlynpickard@gmail.com>
2018-01-23 17:17:39 +1000

    setUserName function is now working

--- server/users.js
+++ server/users.js
@@ -87,64 +87,89 @@ let getExpiringNames = (req, res) => {
 	});
 }
 
+function validateNewName(userID, newFirstName, newLastName, newNameStartDate) {
+	return new Promise((resolve, reject) => {
+		// Check that the user does not already have a name queued
+		let result = db.query('SELECT * FROM public."NAMES" WHERE "USERID" = $1 AND "STARTDATE" > $2', [userID, todaysDate])
+		.then(result => {
+			if (result.length == 0) {
+				// Check that the user has not previously used this name
+				let result = db.query('SELECT * FROM public."NAMES" WHERE "USERID" = $1 AND "FIRSTNAME" = $2 AND "LASTNAME" = $3', [userID, newFirstName, newLastName])
+				.then(result => {
+					if (result.length == 0) {
+						// Check if any other users have this name on the date it will start being in use
+						let result = db.query('SELECT * FROM public."NAMES" WHERE "FIRSTNAME" = $1 AND "LASTNAME" = $2 AND "STARTDATE" <= $3 AND "ENDDATE" >= $3', [newFirstName, newLastName, newNameStartDate])
+						.then(result => {
+							if (result.length == 0) {
+								resolve();
+							} else {
+								reject("ERROR - This name is taken for the period in which you wish to use it.");
+							}
+					    })
+						.catch(function () {
+							console.log("Promise Rejected");
+						});
+					} else {
+						reject("ERROR - You have already used this name previously.");
+					}
+			    })
+				.catch(function () {
+					console.log("Promise Rejected");
+				});
+			} else {
+				reject("ERROR - You already have a name queued.");
+			}
+		})
+		.catch(function () {
+			console.log("Promise Rejected");
+		});
+	});
+}
+
 let setUserName = (req, res) => {
 	let userID = req.params.userID;
 	let newFirstName = req.params.firstName;
 	let newLastName = req.params.lastName;
-	let newNameStartDate = (typeof req.params.startDate === 'undefined') ? todaysDate : new Date(req.params.startDate);
-	let newNameEndDate = (typeof req.params.startDate === 'undefined') ? todaysDate : new Date(req.params.startDate);
-	let oldNameEndDate = (typeof req.params.startDate === 'undefined') ? todaysDate : new Date(req.params.startDate);
+	let newNameStartDate = (typeof req.params.startDate === 'undefined') ? new Date(tomorrowsDate.toString()) : new Date(req.params.startDate);
+
+	let numDaysCanQueue = 7;
+	let maxStartDate = new Date(todaysDate.toString());
+	maxStartDate.setDate(maxStartDate.getDate() + numDaysCanQueue);
+
+	if (newNameStartDate > maxStartDate) {
+		return res.send("ERROR - start date is more than " + numDaysCanQueue + " days in the future.");
+	}
 
 	let numDaysNameIsValid = 364;
-	// let newNameEndDate = new Date(newNameStartDate.getDate());
+	let newNameEndDate = new Date(newNameStartDate.toString());
 	newNameEndDate.setDate(newNameEndDate.getDate() + numDaysNameIsValid);
 	
-	// let oldNameEndDate = new Date(newNameStartDate.getDate());
+	let oldNameEndDate = new Date(newNameStartDate.toString());
 	oldNameEndDate.setDate(oldNameEndDate.getDate() - 1);
 
-	console.log(newNameStartDate);
-	console.log(newNameEndDate);
-	console.log(oldNameEndDate);
-
-	// check that the start date the user has selected is no more than 7 days in the future
-	// TODO
-
-	// check that the user has not previously used this name
-	// TODO
-
-	// check that the user does not already have a new name queued
-	// TODO
-
-	// check no one else has the new name currently
-	let result = db.query('SELECT names.* FROM public."NAMES" names \
-		INNER JOIN (SELECT "USERID", MAX("ENDDATE") AS MaxEndDate FROM public."NAMES" WHERE "FIRSTNAME" = $1 AND "LASTNAME" = $2 GROUP BY "USERID") latestnames \
-		ON names."USERID" = latestnames."USERID" AND names."ENDDATE" = latestnames.MaxEndDate', [newFirstName, newLastName])
+	let validation = validateNewName(userID, newFirstName, newLastName, newNameStartDate)
 	.then(result => {
-		if (result.length == 0) {
-			// Update the entry for the users old name so that it is set to end the day before the new name starts
-			let updateOldName = db.query('UPDATE public."NAMES" SET "ENDDATE" = $1 WHERE "USERID" = $2 AND "ENDDATE" = \
-				(SELECT MAX("ENDDATE") FROM public."NAMES" WHERE "USERID" = $2)', [oldNameEndDate, userID])
+		// Update the user's old name so that it is set to end the day before their new name starts
+		let updateOldName = db.query('UPDATE public."NAMES" SET "ENDDATE" = $1 WHERE "USERID" = $2 AND "ENDDATE" = \
+			(SELECT MAX("ENDDATE") FROM public."NAMES" WHERE "USERID" = $2)', [oldNameEndDate, userID])
+		.then(result => {
+			// Create an entry for the new name to start on the day defined by the user
+			let createNewName = db.query('INSERT INTO public."NAMES"(\
+				"USERID", "FIRSTNAME", "LASTNAME", "STARTDATE", "ENDDATE") \
+				VALUES ($1, $2, $3, $4, $5);', [userID, newFirstName, newLastName, newNameStartDate, newNameEndDate])
 			.then(result => {
-				// Create an entry for the new name to start on the day defined by the user
-				let createNewName = db.query('INSERT INTO public."NAMES"(\
-					"USERID", "FIRSTNAME", "LASTNAME", "STARTDATE", "ENDDATE") \
-					VALUES ($1, $2, $3, $4, $5);', [userID, newFirstName, newLastName, newNameStartDate, newNameEndDate])
-				.then(result => {
-					return res.send("New name has been successfully queued");
-				})
-				.catch(function () {
-					console.log("Promise Rejected");
-				});
+				return res.send("New name has been successfully queued");
 			})
 			.catch(function () {
 				console.log("Promise Rejected");
 			});
-		} else {
-			return res.send("This name is currently taken");
-		}
-    })
-	.catch(function () {
-		console.log("Promise Rejected");
+		})
+		.catch(function () {
+			console.log("Promise Rejected");
+		});
+	})
+	.catch(function (error) {
+		console.log(error);
 	});
 }
 
@@ -154,18 +179,3 @@ exports.getUserQueuedName = getUserQueuedName;
 exports.checkIfCurrentNameExpired = checkIfCurrentNameExpired;
 exports.getExpiringNames = getExpiringNames;
 exports.setUserName = setUserName;
-
-
-
-// let getUser = (req, res) => {
-// 	let id = req.params.id;
-// 	console.log(id)
-
-// 	let result = db.query('SELECT * FROM public."NAMES" WHERE "USERID" = $1', [id])
-// 		.then(result => {
-//             return res.send(result[0].FIRSTNAME);
-//         })
-// 		.catch(function () {
-//  			console.log("Promise Rejected");
-// 		});
-// }
